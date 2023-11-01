7a88d7ea5dc5ca3fa1ad9d24b36da40edbdde82b
B C Samrudh <samrudhbc@gmail.com>
2023-11-01 08:49:15 +0530

    Added the backend for User Authentication

---
+++ routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { signOut, signin, signup } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.get('/signout', signOut)
+
+export default router;
