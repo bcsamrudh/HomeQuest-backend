8a03116dc794f766b95f445b1982b1b42597c721
B C SAMRUDH <114090255+bcsamrudh@users.noreply.github.com>
2024-04-19 19:05:50 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,12 +3,6 @@ import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import Listing from '../models/listing.model.js';
 
-export const test = (req, res) => {
-  res.json({
-    message: 'Api route is working!',
-  });
-};
-
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
     return next(errorHandler(401, 'You can only update your own account!'));
